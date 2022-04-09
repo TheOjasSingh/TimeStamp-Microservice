8bc0842a9a7b2529cf48425003689a1b44c5ad7b
Ojas Singh <ojas856@gmail.com>
2022-04-09 20:45:20 +0530

    Adding Serive.js
    Adding service.js and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+The API endpoint is GET [url]/api/timestamp/:date_string? If the date string is empty the service uses the current timestamp. If the date string is valid the api returns a JSON e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}. API accepts unix time. If the date string is invalid the api returns a JSON having the structure {"error" : "Invalid Date" }.
+
+Example usage:
+https://savory-desert-salesman.glitch.me/api/timestamp/2015-12-25
+https://savory-desert-salesman.glitch.me/api/timestamp/1451001600000
+Example output:
+{"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date?", (req, res)=>{
+  const givenDate = req.params.date;
+  let date;
+
+  if(!givenDate){
+    date = new Date();
+  }else{
+    const checkUnix = gievnDate * 1;
+    date = isNaN(checkUnix) ? new Date(givenDate): new Date(checkUnix);
+  }
+
+  if(date == "Invalid Date"){
+    re.json({error: "Invalid Date"});
+  }else{
+    const unix=date.getTime();
+    const utc= date.toUTCString();
+    re.json({unix, utc});
+  }
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
