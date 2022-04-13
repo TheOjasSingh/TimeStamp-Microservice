a069060c9a8d03cec9998303736f083d2cd5835f
Ojas Singh <ojas856@gmail.com>
2022-04-13 23:21:26 +0530

    JSON to return empty date string
    JSON returns current timestamp, if the date_str is empty

--- server.js
+++ server.js
@@ -18,27 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.get("/api", (req, res) =>{
+  const date = new Date();
+  res.json({
+    "unix":date.getTime(),
+    "utc":date.toUTCString()
+  });
+});
 // your first API endpoint... 
-app.get("/api/timestamp/:date?", (req, res)=>{
-  const givenDate = req.params.date;
-  let date;
+app.get("/api/:date_str", function (req, res) {
+  const{date_str}= req.params;
+  let date = new Date(date_str);
 
-  if(!givenDate){
-    date = new Date();
-  }else{
-    const checkUnix = gievnDate * 1;
-    date = isNaN(checkUnix) ? new Date(givenDate): new Date(checkUnix);
+  if(date.toString()==='Invalid Date'){
+    date = new Date(parseInt(date_str));
   }
-
-  if(date == "Invalid Date"){
-    re.json({error: "Invalid Date"});
+  if(date.toString()==='Invalid Date'){
+    return res.json({
+      "error": "Invalid Date"
+    });
   }else{
-    const unix=date.getTime();
-    const utc= date.toUTCString();
-    re.json({unix, utc});
+    return res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    })
   }
 });
+
 
 
 // listen for requests :)
